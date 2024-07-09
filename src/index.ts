04f4fea87b68efd215701df258e38ad8282803dc
anhvuDev103 <anhvu.dev.103@gmail.com>
2024-07-09 09:44:05 +0700

    wrapRequestHandler

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,12 @@ app.use('/users', usersRouter);
 
 databaseService.connect();
 
+app.use((error: any, req: any, res: any, next: any) => {
+  res.json({
+    ahihi: error.message,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,16 +2,18 @@ import express from 'express';
 
 import { loginController, registerController } from '@/controllers/users.controllers';
 import { registerValidator } from '@/middlewares/users.middlewares';
+import { wrapRequestHandler } from '@/utils/handlers';
 
 const usersRouter = express.Router();
 
 usersRouter.post('/login', loginController);
+
 /**
  * Description: Register a new user
  * Path: /register
  * Method: POST
  * Body: { name: string, email: string, password: string, confirm_password: string, date_of_birth: ISO Date }
  */
-usersRouter.post('/register', registerValidator, registerController);
+usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController));
 
 export default usersRouter;
